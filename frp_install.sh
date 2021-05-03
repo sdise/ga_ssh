5fb1813298b0b287dd8f5a4c475d4faebe17a889
sdise <33455369+sdise@users.noreply.github.com>
2021-05-04 04:30:11 +0800

    Update frp_install.sh

--- frp_install.sh
+++ frp_install.sh
@@ -10,16 +10,19 @@ cd /home/runner
 sudo apt install nginx python3 python3-pip curl wget telnet tar gzip -y
 
 cd /home.runner 
-curl https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_amd64.tar.gz -O /home/runner && cd /home/runner && tar -zxf *gz && rm *gz* && mv frp* frp
+wget https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_amd64.tar.gz -P /home/runner
+cd /home/runner
+sudo chmod 755 /home/runner/frp_0.36.2_linux_amd64.tar.gz
+tar -zxf *gz && rm *gz* && mv frp* frp
 
 curl https://raw.githubusercontent.com/sdise/ga_ssh/main/frpc.ini -o /home/runner/frp/frpc.ini
 
 sudo chmod 755 /home/runner/frp/frpc
  
-bash /home/runner/frp/frpc -c /home/runner/frp/frpc.ini&
+bash /home/runner/frp/frpc -c /home/runner/frp/frpc.ini &
 
 sudo pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip
 
 wget https://raw.githubusercontent.com/sdise/ga_ssh/main/ss.json && mv ss.json /home/runner/
 
-sudo ssserver -c ~/ss.json -d start
+sudo ssserver -c ~/ss.json -d start &
