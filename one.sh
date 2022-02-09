3fa81e241bc56342b15ad4fcf3037ec0c0f98800
sdise <33455369+sdise@users.noreply.github.com>
2022-02-09 16:40:35 +0800

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,9 +1,14 @@
 
-#!/bin/bash                                                                        #
+#!/bin/bash
+#
 echo ”install epel elrepo"
 sleep 2s
-yum install epel-release elrepo-release -y && yum update -y                        echo "install python pip nginx vim wget curl"
+yum install epel-release elrepo-release -y && yum update -y
+
+echo "install python pip nginx vim wget curl"
+sleep 1s
 yum install python3 python3-pip git nginx vim wget curl screen -y
+
 echo "update sysytem kenerl forom elrepo-ml"
 sleep 3s
 yum --enablerepo=elrepo-kernel install kernel-ml -y &&grub2-set-default 0 &&grub2-mkconfig -o /boot/grub2/grub.cfg
@@ -15,11 +20,13 @@ wget -N --no-check-certificate "https://raw.githubusercontent.com/mack-a/v2ray-a
 
 echo "open bbr"
 sleep 1s
-echo "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf                                                                             
-touch .vimrc
-echo "set encoding=utf-8" >> /root/.vimrc                                          
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+exho "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf                                                                    
+#vimrc
+wget  https://raw.githubusercontent.com/sdise/onekeyscripts/main/.vimrc -O $HOME/.vimrc                                   
 sleep 10s
 echo "### 运行完成 ###"
-read isreboot
-
+#read isreboot
+echo "30秒后自动重启*
+sleep 30s
 reboot
